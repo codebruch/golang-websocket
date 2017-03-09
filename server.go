57198f19ee32eb935a6408eb5a3695a073e9ab47
Charles Hansen <chansen@pivotal.io>
2017-03-09 14:50:49 -0800

    Add sample golang websocket server and Js client
    Signed-off-by: Shash Reddy <sreddy@pivotal.io>

---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/websocket"
+)
+
+func Echo(ws *websocket.Conn) {
+	var err error
+
+	for {
+		var reply string
+
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+
+		fmt.Println("Received back from client: " + reply)
+
+		msg := "Received:  " + reply
+		fmt.Println("Sending to client: " + msg)
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.Handle("/", websocket.Handler(Echo))
+
+	port := os.Getenv("PORT")
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
